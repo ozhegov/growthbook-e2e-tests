c976561ae981106a0cce971d31af93d89a1b42eb
Maksim Ozhegov <qa.ozhegov@gmail.com>
2026-01-08 15:33:05 +0300

    init commit

---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests',
+    testMatch: /.*\.test\.ts/,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 3 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [
+        ['line'],
+        ['allure-playwright', { resultsDir: 'allure-results' }],
+      ],
+    use: {
+        baseURL: process.env.BASE_URL || 'http://localhost:3000',
+        trace: 'retain-on-failure',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
+    ],
+});
